156917c9d22c1ac8268490c8f20a96ef56689ef7
Curtis Bilbrey <pancakemix007@gmail.com>
2014-02-25 16:52:27 -0700

    Lists and game grid

--- src/com/smashdev/ssmash/Board.java
+++ src/com/smashdev/ssmash/Board.java
@@ -6,88 +6,129 @@ import java.util.Random;
 
 public class Board {
 	
+	static List<Integer> R1 = new ArrayList<Integer>();
+	static List<Integer> R2 = new ArrayList<Integer>();
+	static List<Integer> R3 = new ArrayList<Integer>();
+	static List<Integer> R4 = new ArrayList<Integer>();
+	static List<Integer> R5 = new ArrayList<Integer>();
+	static List<Integer> R6 = new ArrayList<Integer>();
+	static List<Integer> R7 = new ArrayList<Integer>();
+	static List<Integer> R8 = new ArrayList<Integer>();
+	static List<Integer> R9 = new ArrayList<Integer>();
+	
+	static List<Integer> C1 = new ArrayList<Integer>();
+	static List<Integer> C2 = new ArrayList<Integer>();
+	static List<Integer> C3 = new ArrayList<Integer>();
+	static List<Integer> C4 = new ArrayList<Integer>();
+	static List<Integer> C5 = new ArrayList<Integer>();
+	static List<Integer> C6 = new ArrayList<Integer>();
+	static List<Integer> C7 = new ArrayList<Integer>();
+	static List<Integer> C8 = new ArrayList<Integer>();
+	static List<Integer> C9 = new ArrayList<Integer>();
+	
+	static List<Integer> G1 = new ArrayList<Integer>();
+	static List<Integer> G2 = new ArrayList<Integer>();
+	static List<Integer> G3 = new ArrayList<Integer>();
+	static List<Integer> G4 = new ArrayList<Integer>();
+	static List<Integer> G5 = new ArrayList<Integer>();
+	static List<Integer> G6 = new ArrayList<Integer>();
+	static List<Integer> G7 = new ArrayList<Integer>();
+	static List<Integer> G8 = new ArrayList<Integer>();
+	static List<Integer> G9 = new ArrayList<Integer>();
+	
 	public Board() {
 		
-		int[][] solution;
+		for (int i = 0; i <= 8; i++) {
+			R1.add(i, i+1);
+			C1.add(i, i+1);
+			G1.add(i, i+1);
+			R2.add(i, i+1);
+			C2.add(i, i+1);
+			G2.add(i, i+1);
+			R3.add(i, i+1);
+			C3.add(i, i+1);
+			G3.add(i, i+1);
+			R4.add(i, i+1);
+			C4.add(i, i+1);
+			G4.add(i, i+1);
+			R5.add(i, i+1);
+			C5.add(i, i+1);
+			G5.add(i, i+1);
+			R6.add(i, i+1);
+			C6.add(i, i+1);
+			G6.add(i, i+1);
+			R7.add(i, i+1);
+			C7.add(i, i+1);
+			G7.add(i, i+1);
+			R8.add(i, i+1);
+			C8.add(i, i+1);
+			G8.add(i, i+1);
+			R9.add(i, i+1);
+			C9.add(i, i+1);
+			G9.add(i, i+1);
+		}
 		
-		List<List<Integer>> possNums = new ArrayList<List<Integer>>(27);
-		List<Integer> R1 = new ArrayList<Integer>();
-		List<Integer> R2 = new ArrayList<Integer>();
-		List<Integer> R3 = new ArrayList<Integer>();
-		List<Integer> R4 = new ArrayList<Integer>();
-		List<Integer> R5 = new ArrayList<Integer>();
-		List<Integer> R6 = new ArrayList<Integer>();
-		List<Integer> R7 = new ArrayList<Integer>();
-		List<Integer> R8 = new ArrayList<Integer>();
-		List<Integer> R9 = new ArrayList<Integer>();
+		shuffleList(R1);
+		shuffleList(R2);
+		shuffleList(R3);
+		shuffleList(R4);
+		shuffleList(R5);
+		shuffleList(R6);
+		shuffleList(R7);
+		shuffleList(R8);
+		shuffleList(R9);
 		
-		List<Integer> C1 = new ArrayList<Integer>();
-		List<Integer> C2 = new ArrayList<Integer>();
-		List<Integer> C3 = new ArrayList<Integer>();
-		List<Integer> C4 = new ArrayList<Integer>();
-		List<Integer> C5 = new ArrayList<Integer>();
-		List<Integer> C6 = new ArrayList<Integer>();
-		List<Integer> C7 = new ArrayList<Integer>();
-		List<Integer> C8 = new ArrayList<Integer>();
-		List<Integer> C9 = new ArrayList<Integer>();
+		shuffleList(C1);
+		shuffleList(C2);
+		shuffleList(C3);
+		shuffleList(C4);
+		shuffleList(C5);
+		shuffleList(C6);
+		shuffleList(C8);
+		shuffleList(C8);
+		shuffleList(C9);
 		
-		List<Integer> G1 = new ArrayList<Integer>();
-		List<Integer> G2 = new ArrayList<Integer>();
-		List<Integer> G3 = new ArrayList<Integer>();
-		List<Integer> G4 = new ArrayList<Integer>();
-		List<Integer> G5 = new ArrayList<Integer>();
-		List<Integer> G6 = new ArrayList<Integer>();
-		List<Integer> G7 = new ArrayList<Integer>();
-		List<Integer> G8 = new ArrayList<Integer>();
-		List<Integer> G9 = new ArrayList<Integer>();
-		
-		possNums.set(0, R1);
-		possNums.set(1, R2);
-		possNums.set(2, R3);
-		possNums.set(3, R4);
-		possNums.set(4, R5);
-		possNums.set(5, R6);
-		possNums.set(6, R7);
-		possNums.set(7, R8);
-		possNums.set(8, R9);
-		
-		possNums.set(9, C1);
-		possNums.set(10, C2);
-		possNums.set(11, C3);
-		possNums.set(12, C4);
-		possNums.set(13, C5);
-		possNums.set(14, C6);
-		possNums.set(15, C7);
-		possNums.set(16, C8);
-		possNums.set(17, C9);
-		
-		possNums.set(18, G1);
-		possNums.set(19, G2);
-		possNums.set(20, G3);
-		possNums.set(21, G4);
-		possNums.set(22, G5);
-		possNums.set(23, G6);
-		possNums.set(24, G7);
-		possNums.set(25, G8);
-		possNums.set(26, G9);
-		
-		populatePossNums(possNums);
-		shufflePossNums(possNums);
+		shuffleList(G1);
+		shuffleList(G2);
+		shuffleList(G3);
+		shuffleList(G4);
+		shuffleList(G5);
+		shuffleList(G6);
+		shuffleList(G7);
+		shuffleList(G8);
+		shuffleList(G9);
 	}
 	
-	public void populatePossNums(List<List<Integer>> pPossNums) {
-		for (int i = 0; i <= 8; i++) {
-			List<Integer> tempList = pPossNums.get(i);
-			tempList.add(i,i + 1);
-		}
-		for (int i = 9; i <= 17; i++) {
-			List<Integer> tempList = pPossNums.get(i);
-			tempList.add(i,i + 1);
-		}
-		for (int i = 18; i <= 26; i++) {
-			List<Integer> tempList = pPossNums.get(i);
-			tempList.add(i,i + 1);
-		}
+	public void populatePossNums() {
+/*			R1.add(0, 1);
+			R2.add(1, 2);
+			R3.add(2, 3);
+			R4.add(3, 4);
+			R5.add(4, 5);
+			R6.add(5, 6);
+			R7.add(6, 7);
+			R8.add(7, 8);
+			R9.add(8, 9);
+			
+			C1.add(0, 1);
+			C2.add(1, 2);
+			C3.add(2, 3);
+			C4.add(3, 4);
+			C5.add(4, 5);
+			C6.add(5, 6);
+			C7.add(6, 7);
+			C8.add(7, 8);
+			C9.add(8, 9);
+			
+			G1.add(0, 1);
+			G2.add(1, 2);
+			G3.add(2, 3);
+			G4.add(3, 4);
+			G5.add(4, 5);
+			G6.add(5, 6);
+			G7.add(6, 7);
+			G8.add(7, 8);
+			G9.add(8, 9);*/
 	}
 	
 	public static void shuffleList(List<Integer> nums) {
@@ -106,13 +147,11 @@ public class Board {
 		nums.set(change, helper);
 	}
 	
-	public static void shufflePossNums(List<List<Integer>> sPossNums) {
-		for (int i = 0; i <= 26; i++) {
-			shuffleList(sPossNums.get(i));
-		}	
-	}
+/*	public static int getNumber(int index) {
+		
+	}*/
 	
-	private int[][] generateSolution(int[][] game, int index) {
+/*	private int[][] generateSolution(int[][] game, int index) {
 		for (int i = 0 ; i <= 8 ; i++) {
 			for (int j = 0 ; j <= 8 ; j++) {
 				generateNextNumber();
@@ -122,6 +161,6 @@ public class Board {
 	
 	private int generateNextNumber(int[][] board, int x, int y) {
 		
-	}
+	}*/
 
 }
--- src/com/smashdev/ssmash/Game.java
+++ src/com/smashdev/ssmash/Game.java
@@ -3,16 +3,26 @@ package com.smashdev.ssmash;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.GridView;
 import android.app.Activity;
 
 
 public class Game extends Activity {
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
+		
+		
+		Board someBoard = new Board();
+		
+		
+		
+		
 		
 	}
 }
